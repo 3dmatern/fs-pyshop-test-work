24bca11d982b86c28da9899a539581c834504c08
Denis Matern <denismatern@gmail.com>
2024-04-02 17:03:36 +0300

    feat: delete profile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 
 import { UsersService } from './users.service';
+import { AppGuard } from 'src/app.guard';
+import { ParseToken } from 'src/token/token.module';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(AppGuard)
+  @HttpCode(HttpStatus.OK)
+  @Delete()
+  deleteUser(@Request() req) {
+    const user: ParseToken = req.user;
+
+    return this.usersService.delete({ id: user.sub });
+  }
 }
