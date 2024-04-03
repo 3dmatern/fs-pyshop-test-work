4b4b59bed7bd34c35a1e19f05bff9e66e68a59ff
Denis Matern <denismatern@gmail.com>
2024-04-03 11:18:10 +0300

    feat: env cors origin

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+  const frontendPyShop = configService.get('CORS_ORIGIN_FRONTEND_PYSHOP');
 
-  app.enableCors();
+  app.enableCors({
+    origin: frontendPyShop,
+  });
   app.setGlobalPrefix('api');
   await app.listen(port);
 }
