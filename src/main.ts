f4bfcf383234ad484f928833473c312d2d1d2af6
Denis Matern <denismatern@gmail.com>
2024-04-03 19:42:45 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
-  const frontendPyShop = configService.get('CORS_ORIGIN_FRONTEND_PYSHOP');
+  // const frontendPyShop = configService.get('CORS_ORIGIN_FRONTEND_PYSHOP');
 
-  app.enableCors({
-    origin: frontendPyShop,
-  });
+  // app.enableCors({
+  //   origin: frontendPyShop,
+  // });
+  app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(port);
 }
